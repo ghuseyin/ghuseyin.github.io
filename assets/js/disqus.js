3fe22b77fd1ec7d120f0b194d8f42d3b1900f427
Hüseyin GÜZEL <ghuseyin@yandex.com>
2019-09-15 07:58:17 -0400

    Delete disqus.js

--- assets/js/disqus.js
+++
@@ -1,6 +0,0 @@
-(async function() {
-          var d = document, s = d.createElement('script');
-          s.src = 'https://huseyinguzel-web-tr.disqus.com/embed.js' defer;
-          s.setAttribute('data-timestamp', +new Date());
-          (d.head || d.body).appendChild(s);
-})();
