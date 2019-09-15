cc8b989c8dad81b2f76c069f77b0c5dbd4eb62db
Hüseyin GÜZEL <ghuseyin@yandex.com>
2019-09-15 07:40:55 -0400

    Update disqus.js

--- assets/js/disqus.js
+++ assets/js/disqus.js
@@ -1,4 +1,4 @@
-(function() {
+(async function() {
           var d = document, s = d.createElement('script');
           s.src = 'https://huseyinguzel-web-tr.disqus.com/embed.js' defer;
           s.setAttribute('data-timestamp', +new Date());
